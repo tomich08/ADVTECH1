4fd8107d25b022a90cc33e71a89ebc6e15f1ac44
tomich08 <mmt8@students.pti.edu>
2014-10-16 08:44:38 -0400

    demo change

--- Demo3/js/script.js
+++ Demo3/js/script.js
@@ -1,14 +1,21 @@
 var username = "Fred";
 
-document.onclick = 
+//languages offer other useful events than onclick
+
+document.onmousemove = 
 	function(evt){
 		makeOneCircle(evt);
 	}
 
 	function makeOneCircle(evt){
-		evt= (evt||event)
-
+		evt = (evt||event)
+		//makes all browsers understand the event
+		//make a new circle
 		var newc = document.createElement("div");
-		document.appendChild(newc);
+		//set location of the new div to where the mouse is
+		newc.style.left = evt.clientX+"px";
+		newc.style.top = evt.clientY+"px";
+		//add "body" below
+		document.body.appendChild(newc);
 
 	}
